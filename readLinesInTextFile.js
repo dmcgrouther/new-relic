8edab90a9ccd8ba9bd7930e7092149ee4b75ad3e
david-mcgrouther <dbmcgrouther@gmail.com>
2021-06-30 10:30:58 -0700

    able to loop through lines of text file and keep count after each line

--- readLinesInTextFile.js
+++ readLinesInTextFile.js
@@ -4,11 +4,32 @@ const lineReader = require('line-reader');
 
 const mostCommonThreeWords = (route) => {
 
+  let threeWordHash = {};
+
+  let count = 0;
+  let newString = '';
+
   lineReader.eachLine(route, function(line) {
-    console.log(line);
-  })
+    let splitLine = line.split(' ');
+    console.log(`new line... splitLine is ${splitLine}`);
+    for(let i = 0; i < splitLine.length; i++){
+      count++;
+      console.log(`splitLine[i] is ${splitLine[i]} count is ${count}`);
+
+      if(count === 1){
+        newString = splitLine[i];
+      } else if(count === 2) {
+        newString = newString + ' ' + splitLine[i];
+      } else if(count === 3){
+        newString = newString + ' ' + splitLine[i];
+        console.log(newString);
+        count = 0;
+        newString = '';
+      }
+    }
+  });
   
-}
+};
 
 
 mostCommonThreeWords('./stuff.txt');
